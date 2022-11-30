a4dd693cbd9b40fa41d80b7a1c876d94d489e77f
NaufalPujiMahdy <naufalpm230800@gmail.com>
2022-11-30 16:17:22 +0700

    first commit

---
+++ app/Http/Controllers/NewsController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\NewsCollection;
+use App\Models\news;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class NewsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $news = new NewsCollection(News::paginate(8));
+        return Inertia::render('Homepage', [
+            'title' => 'CUY UNIVERSE HOME',
+            'description' => 'Selamat Datang di Cuy Universe News Portal',
+            'news' => $news,
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\news  $news
+     * @return \Illuminate\Http\Response
+     */
+    public function show(news $news)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\news  $news
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(news $news)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\news  $news
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, news $news)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\news  $news
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(news $news)
+    {
+        //
+    }
+}
